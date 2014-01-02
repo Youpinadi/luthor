abeb6fa2e99d995a27607565522a687878019636
Nadir Kadem <nadir.kadem@gmail.com>
2014-01-03 00:55:37 +0100

    fix test input

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function MainCtrl($scope, $sce) {
     $scope.tokens = [];
     $scope.mini_whitespaces = false;
 
-    $scope.input = "variable1 >\t5;\ni == 2;\n1 + 2 = 3;\ntoto = \"pouet\"; yo = 'hello'";
+    $scope.input = "variable1 >\t5;\ni == 2;\n1 + 2 = 3;\ntoto = \"pouet\";\nyo = 'hello'";
 
     $scope.findTokens = function()
     {
